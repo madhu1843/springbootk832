7d1146229644a8c3f852fee2c11f515584109eea
hp <rnaman51@gmail.com>
2023-01-27 13:12:21 +0530

    springbootk83

---
+++ springbootk83/src/main/java/com/springbootk83/springbootk83/Springbootk83Application.java
@@ -0,0 +1,13 @@
+package com.springbootk83.springbootk83;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootk83Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootk83Application.class, args);
+	}
+
+}
---
+++ springbootk83/src/test/java/com/springbootk83/springbootk83/Springbootk83ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootk83.springbootk83;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootk83ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
